4d1bcd54873a6ccc4348b1a8ff1bacf41358b683
Ilya Shpigor <petrsum@gmail.com>
2019-04-05 23:06:00 +0200

    Add the feature for choosing the active mode for the picker.py script

--- picker.py
+++ picker.py
@@ -14,8 +14,17 @@ _GREEN_COLOR = "#66ce54"
 _YELLOW_COLOR = "#f9ef31"
 _RED_COLOR = "#ff4f4f"
 
-HEROES = {}
+_INDEX_COLORS = {
+  0 : _RED_COLOR,
+  1 : _GREEN_COLOR,
+  2 : _AZURE_COLOR}
 
+INDEX_BUTTONS = {}
+
+SELECTED_HERO = None
+ACTIVE_INDEX = None
+
+HEROES = {}
 BUTTONS = {}
 
 def load_heroes():
@@ -30,7 +39,17 @@ def reset_all_buttons():
   for key, value in BUTTONS.iteritems():
     value[0].config(bg = _DEFAULT_COLOR)
 
-def highlight_related_heroes(hero_name, index, color):
+def reset_highlight():
+  global _YELLOW_COLOR
+  global BUTTONS
+  global SELECTED_HERO
+
+  reset_all_buttons()
+
+  BUTTONS[SELECTED_HERO][0].config(bg = _YELLOW_COLOR)
+
+def highlight_related_heroes(hero_name, index):
+  global _INDEX_COLORS
   global _DEFAULT_COLOR
   global BUTTON
   global HEROES
@@ -41,26 +60,33 @@ def highlight_related_heroes(hero_name, index, color):
     if key != hero_name:
       if key in related_heroes \
          and value[0].cget("bg") == _DEFAULT_COLOR:
-        value[0].config(bg = color)
+        value[0].config(bg = _INDEX_COLORS[index])
 
-
-def button_click(hero_name):
-  global _RED_COLOR
+def highlight_all_relations(hero_name):
   global _YELLOW_COLOR
-  global _GREEN_COLOR
-  global _AZURE_COLOR
   global BUTTONS
   global HEROES
 
-  reset_all_buttons()
+  reset_highlight()
 
-  BUTTONS[hero_name][0].config(bg = _YELLOW_COLOR)
+  highlight_related_heroes(hero_name, 2)
 
-  highlight_related_heroes(hero_name, 2, _AZURE_COLOR)
+  highlight_related_heroes(hero_name, 0)
 
-  highlight_related_heroes(hero_name, 0, _RED_COLOR)
+  highlight_related_heroes(hero_name, 1)
 
-  highlight_related_heroes(hero_name, 1, _GREEN_COLOR)
+def button_click(hero_name):
+  global SELECTED_HERO
+
+  SELECTED_HERO = hero_name
+
+  if ACTIVE_INDEX == None:
+
+    highlight_all_relations(hero_name)
+  else:
+    reset_highlight()
+
+    highlight_related_heroes(hero_name, ACTIVE_INDEX)
 
 def add_label(window, letter, column, row):
   label = Label(window, text=letter, font=("Arial Bold", 12))
@@ -108,10 +134,45 @@ def add_buttons(window):
 
     column, row = get_next_cell(column, row)
 
+def toggle_index_button(index):
+  global INDEX_BUTTONS
+
+  for key in INDEX_BUTTONS.keys():
+    if key != index:
+      INDEX_BUTTONS[key].config(relief="raised")
+
+  if INDEX_BUTTONS[index].config("relief")[-1] == "sunken":
+      INDEX_BUTTONS[index].config(relief="raised")
+  else:
+      INDEX_BUTTONS[index].config(relief="sunken")
+
+def enable_index(index):
+  global SELECTED_HERO
+  global ACTIVE_INDEX
+
+  if not SELECTED_HERO:
+    return
+
+  toggle_index_button(index)
+
+  if index == ACTIVE_INDEX:
+    ACTIVE_INDEX = None
+    button_click(SELECTED_HERO)
+    return
+  else:
+    ACTIVE_INDEX = index
+
+  reset_all_buttons()
+
+  BUTTONS[SELECTED_HERO][0].config(bg = _YELLOW_COLOR)
+
+  highlight_related_heroes(SELECTED_HERO, index)
+
 def make_window():
   global _RED_COLOR
   global _GREEN_COLOR
   global _AZURE_COLOR
+  global INDEX_BUTTONS
   global VERSION
 
   window = Tk()
@@ -127,14 +188,24 @@ def make_window():
   info_frame.pack(fill = BOTH, expand = True)
 
 
-  color1 = Label(info_frame, bg = _RED_COLOR, width = 4, height = 1)
-  color1.grid(column = 0, row = 0)
+  INDEX_BUTTONS[0] = Button(info_frame)
+  INDEX_BUTTONS[0].grid(column = 0, row = 0)
+  INDEX_BUTTONS[0].config(command = lambda:enable_index(0), \
+                compound = TOP, bg = _RED_COLOR, width = 8, height = 2, \
+                font=("Arial Bold", 5), pady = 0, padx = 0, relief="raised")
 
-  color2 = Label(info_frame, bg = _GREEN_COLOR, width = 4, height = 1)
-  color2.grid(column = 0, row = 1)
+  INDEX_BUTTONS[1] = Button(info_frame)
+  INDEX_BUTTONS[1].grid(column = 0, row = 1)
+  INDEX_BUTTONS[1].config(command = lambda:enable_index(1), \
+                compound = TOP, bg = _GREEN_COLOR, width = 8, height = 2, \
+                font=("Arial Bold", 5), pady = 0, padx = 0, relief="raised")
 
-  color3 = Label(info_frame, bg = _AZURE_COLOR, width = 4, height = 1)
-  color3.grid(column = 0, row = 2)
+
+  INDEX_BUTTONS[2] = Button(info_frame)
+  INDEX_BUTTONS[2].grid(column = 0, row = 2)
+  INDEX_BUTTONS[2].config(command = lambda:enable_index(2), \
+                compound = TOP, bg = _AZURE_COLOR, width = 8, height = 2, \
+                font=("Arial Bold", 5), pady = 0, padx = 0, relief="raised")
 
 
   bad_label = Label(info_frame, font=("Arial Bold", 12), \
