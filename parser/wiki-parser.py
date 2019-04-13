bdc7abf504b1fd2b7e8e7653363c83db054958b9
Ilya Shpigor <petrsum@gmail.com>
2019-04-13 12:12:38 +0200

    Fix the wiki-parser.py script according to the new CSV format

--- parser/wiki-parser.py
+++ parser/wiki-parser.py
@@ -26,57 +26,61 @@ def get_hero_name(line):
   return hero
 
 def process_file(filename):
-    global _BAD_LABEL
-    global _GOOD_LABEL
-    global _SYNERGY_LABEL
+  global _BAD_LABEL
+  global _GOOD_LABEL
+  global _SYNERGY_LABEL
 
-    global BAD_LIST
-    global GOOD_LIST
-    global SYNERGY_LIST
+  global BAD_LIST
+  global GOOD_LIST
+  global SYNERGY_LIST
 
-    BAD_LIST = []
-    GOOD_LIST = []
-    SYNERGY_LIST = []
+  BAD_LIST = []
+  GOOD_LIST = []
+  SYNERGY_LIST = []
 
-    f = open(filename, "r")
-    for line in f:
-        if _BAD_LABEL in line:
-            BAD_LIST.append(get_hero_name(line))
-        elif _GOOD_LABEL_1 in line or _GOOD_LABEL_2 in line:
-            GOOD_LIST.append(get_hero_name(line))
-        elif _SYNERGY_LABEL in line:
-            SYNERGY_LIST.append(get_hero_name(line))
+  f = open(filename, "r")
+  for line in f:
+    if _BAD_LABEL in line:
+      BAD_LIST.append(get_hero_name(line))
+    elif _GOOD_LABEL_1 in line or _GOOD_LABEL_2 in line:
+      GOOD_LIST.append(get_hero_name(line))
+    elif _SYNERGY_LABEL in line:
+      SYNERGY_LIST.append(get_hero_name(line))
 
-def print_hero(hero, filename):
-    global BAD_LIST
-    global GOOD_LIST
-    global SYNERGY_LIST
+def print_header(f):
+  f.write("Hero;Bad against...;Good against...;Works well with...\n")
 
-    f = open(filename, "a+")
+def print_hero(hero, f):
+  global BAD_LIST
+  global GOOD_LIST
+  global SYNERGY_LIST
 
-    # Fix the apostrophe sign in the hero name
-    if "Nature" in hero:
-      hero = "Natures Prophet"
+  # Fix the apostrophe sign in the hero name
+  if "Nature" in hero:
+    hero = "Natures Prophet"
 
-    f.write(hero)
-    f.write(";")
-    f.write(", ".join(BAD_LIST))
-    f.write(";")
-    f.write(", ".join(GOOD_LIST))
-    f.write(";")
-    f.write(", ".join(SYNERGY_LIST))
-    f.write("\n")
+  f.write(hero)
+  f.write(";")
+  f.write(", ".join(BAD_LIST))
+  f.write(";")
+  f.write(", ".join(GOOD_LIST))
+  f.write(";")
+  f.write(", ".join(SYNERGY_LIST))
+  f.write("\n")
 
 def main():
-    if os.path.isfile(_OUT_FILENAME):
-        os.remove(_OUT_FILENAME)
+  if os.path.isfile(_OUT_FILENAME):
+    os.remove(_OUT_FILENAME)
 
-    hero_files = os.listdir(_HTML_DIR)
+  hero_files = os.listdir(_HTML_DIR)
+
+  with open(_OUT_FILENAME, "a+") as f:
+    print_header(f)
 
     for hero_name in sorted(hero_files):
-        process_file(_HTML_DIR + "/" + hero_name)
+      process_file(_HTML_DIR + "/" + hero_name)
 
-        print_hero(hero_name, _OUT_FILENAME)
+      print_hero(hero_name, f)
 
 if __name__ == '__main__':
   main()
