19a9d73d9dbd7c496eb3a670742cb90ea9330e78
Ilya Shpigor <petrsum@gmail.com>
2019-04-15 21:01:54 +0200

    Adapt the csv2pkl.py script to Python3

--- csv2pkl.py
+++ csv2pkl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from csv import reader
 import pickle
@@ -14,7 +14,7 @@ def load_table(filename, table):
     csv_reader = reader(csv_file, delimiter=';')
 
     # Skip the columns headers
-    csv_reader.next()
+    next(csv_reader)
 
     for line in csv_reader:
         table[line[0]] = [line[1].split(', '), line[2].split(', '), line[3].split(', ')]
